2806c840e730fea2219273dc0c04ceae14e4435c
Aan <dalang@petruknisme.com>
2022-09-10 15:17:59 +0700

    Intiail release for unquoted_checker

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # unquoted_checker
 Scanner for finding Unquoted Service Path for Privilege Escalation
+
+# Purpose
+
+This tool is able to find the Unquoted Service Path for doing Privilege Escalation when using `execute-assembly` or run directly on compromised machine.
+
+# Usage
+
+- Using execute assembly
+
+```
+execute-assembly path\to\unquoted_checker.exe
+Checking Unquoted Service Path for Privilege Escalation...
+    Service name:    VulnService
+    Path:            C:\Program Files\Vuln Service\Vuln.exe
+    Started:         False
+    State:           Stopped
+    Status:          OK
+Press any key to continue . . .
+```
+
+- Direct run
+
+```
+C:\User> unquoted_checker.exe
+Checking Unquoted Service Path for Privilege Escalation...
+    Service name:    VulnService
+    Path:            C:\Program Files\Vuln Service\Vuln.exe
+    Started:         False
+    State:           Stopped
+    Status:          OK
+Press any key to continue . . .
+```
+
+
+# License 
+
+MIT License
---
+++ unquoted_checker/Program.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2022 Petruknisme
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unquoted_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUnquotedServices();
+        }
+        private static void GetUnquotedServices()
+        {
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+
+            // Display the list of services currently running on this computer.
+
+            Console.WriteLine("Checking Unquoted Service Path for Privilege Escalation...");
+            foreach (ServiceController scTemp in scServices)
+            {
+
+                ManagementObject wmiService;
+                wmiService = new ManagementObject("Win32_Service.Name='" + scTemp.ServiceName + "'");
+                wmiService.Get();
+                if (wmiService["PathName"] != null)
+                {
+                    string pathName = wmiService["PathName"].ToString();
+                    List<string> cleanPathName = new List<string>(
+                        pathName.Split(new string[] { " -", " /" }, StringSplitOptions.None));
+
+                    if (!cleanPathName[0].Contains("\"") && cleanPathName[0].Contains(" "))
+                    {
+                        Console.WriteLine("    Service name:    {0}", wmiService["Name"]);
+                        Console.WriteLine("    Path:            {0}", wmiService["PathName"]);
+                        Console.WriteLine("    Started:         {0}", wmiService["Started"]);
+                        Console.WriteLine("    State:           {0}", wmiService["State"]);
+                        Console.WriteLine("    Status:          {0}", wmiService["Status"]);
+                    }
+                    
+                }
+
+            }
+        }
+    }
+}
